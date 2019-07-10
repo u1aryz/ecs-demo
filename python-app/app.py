94d4844cfae664181e367d4e18378058d5a0f342
u1aryz <u1aryz.d@gmail.com>
2019-07-10 23:22:00 +0900

    jsonを返すように変更

--- python-app/app.py
+++ python-app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello world"
+    return jsonify({'message': 'Hello World'})
 
 
 if __name__ == "__main__":
