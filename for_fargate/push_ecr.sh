2fa0d048bf7ad53557abc3c081453584836ef95e
u1aryz <u1aryz.d@gmail.com>
2019-07-13 17:00:32 +0900

    Fargate用のコンテナ郡を作成

---
+++ for_fargate/push_ecr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+if [ ! -f $SCRIPT_DIR/.env ]; then
+  echo ".envがありません"
+  exit 1
+fi
+
+export $(grep -v '^#' .env | xargs)
+
+if [ -z $PROXY_REPO -o -z $API_REPO ]; then
+  echo ".envの内容を確認してください"
+  exit 1
+fi
+
+docker-compose -f $SCRIPT_DIR/docker-compose.yml build
+
+$(aws ecr get-login --no-include-email --region ap-northeast-1)
+ecs-cli push $PROXY_REPO:latest
+ecs-cli push $API_REPO:latest
